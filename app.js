fd440acb1a209cd752324891f2139a066b6489ab
Cumbie <bcumbie@una.edu>
2023-08-24 13:31:43 -0500

    did a port var

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const port = (process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
 })
 
 
-app.listen(3000)
+// app.listen(3000)
+
+app.listen(
+  () => console.log(`Server is running...on ${ port }` ));
