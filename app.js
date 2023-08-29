35248bcec6c3cd5bc0412908815a66953cd5db1a
Cumbie <bcumbie@una.edu>
2023-08-29 13:36:11 -0500

    midway through first half of nov

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
-const port = (process.env.PORT || 3000);
+const port = (process.env.PORT || 3000)
+
+app.set('view engine', 'ejs');
+
+let myVariableServer = 'soft coded server data';
+
+app.get('/barry', function (req, res) {
+  res.render('index', 
+  {
+    'myVariableClient' : myVariableServer 
+  }
+  );
+})
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
 })
 
+app.get('/whatever', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
 
 // app.listen(3000)
 
-app.listen(
-  () => console.log(`Server is running...on ${ port }` ));
+app.listen(port, () => console.log(`Server is running...on ${ port }` ));
